a0bcc4cf8cbff73d9b9347d0e1ea8eb6493040eb
VickyBondarenko <quikyv@gmail.com>
2023-02-03 17:13:49 +0100

    Add npm pak

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-const a = 12;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.body;
+let timerId = null;
+btnStop.setAttribute('disabled', true);
+
+changeColor = () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+};
+
+btnStart.addEventListener('click', () => {
+  btnStop.removeAttribute('disabled');
+  btnStart.setAttribute('disabled', true);
+  timerId = setInterval(changeColor, 1000);
+  console.log('start');
+});
+
+btnStop.addEventListener('click', () => {
+  btnStop.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+  console.log('stop');
+});
