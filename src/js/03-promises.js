d20a900a0d407321895f26d90440faf75de5dcb9
VickyBondarenko <quikyv@gmail.com>
2023-02-04 21:07:56 +0100

    Remove icons

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,11 +17,11 @@ function onSubmit(event) {
 
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   }
